e8dcd520d6b239af085aacd0c715807ef61ee402
Machoudi2002 <stehat982@gmail.com>
2023-11-09 20:26:01 +0100

    stock cart config

--- src/Components/other/CartItem.tsx
+++ src/Components/other/CartItem.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { useShoppingCart } from "../../Context/ShoppingCartContext"
 import { Link } from 'react-router-dom'
 import { productInfo } from './ProductList'
@@ -14,29 +14,29 @@ type CartItemProps = {
 
 
 const CartItem = ({id, quantity} : CartItemProps) => {
-    const { addProductQuantity, minusProductQuantity, removeFromCart} = useShoppingCart();
-
+    const { addProductQuantity, minusProductQuantity, removeFromCart, getProductQuantity} = useShoppingCart();
 
     const item = productInfo.find(item => item.id === id)
     if (item == null) return null
+    const stockCalcul =  item.stock - getProductQuantity(item.id);
+
+
   return (
     <>
         <div className="itemCard">
             <Link to={`/Products/${item.name}`} className="img-back">
                 <img src={item.imgURL} alt={item.name} />
             </Link>
-            <div className="item-info">
-                <div className="text-area">
-                    <h3>{item.name}</h3>
-                    <p>{item.price}$ { quantity > 1 && (<span> x{quantity}</span>)}</p>
-                </div>
-                <div className="selectQuantity">
-                    <button onClick={() => minusProductQuantity(item.id)}>-</button>
-                    <button onClick={() => addProductQuantity(item.id, 1)}>+</button>
-                    <button onClick={() => removeFromCart(item.id)} className="remove"><FontAwesomeIcon icon={faTrash} /></button>
-                </div>
+            <div className="text-area">
+                <h3>{item.name}</h3>
+                <p>{item.price}$</p>
+            </div>                   
+            <div className="controllQuantity">
+                <button onClick={() => minusProductQuantity(item.id)}>-</button>
+                <span>{quantity}</span>
+                <button onClick={() => addProductQuantity(item.id, 1)} disabled={(stockCalcul === 0)}>+</button>
             </div>
-
+            <FontAwesomeIcon onClick={() => removeFromCart(item.id)} className="remove" icon={faXmark} />
         </div>
     </>
   )
