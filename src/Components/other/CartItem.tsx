026ebea8fd756695e1723d5fddb480fc180085ba
Machoudi2002 <stehat982@gmail.com>
2023-11-11 13:03:48 +0100

    cart design

--- src/Components/other/CartItem.tsx
+++ src/Components/other/CartItem.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { useShoppingCart } from "../../Context/ShoppingCartContext"
 import { Link } from 'react-router-dom'
 import { productInfo } from './ProductList'
@@ -24,19 +24,27 @@ const CartItem = ({id, quantity} : CartItemProps) => {
   return (
     <>
         <div className="itemCard">
-            <Link to={`/Products/${item.name}`} className="img-back">
-                <img src={item.imgURL} alt={item.name} />
-            </Link>
-            <div className="text-area">
-                <h3>{item.name}</h3>
-                <p>{item.price}$</p>
-            </div>                   
-            <div className="controllQuantity">
-                <button onClick={() => minusProductQuantity(item.id)}>-</button>
-                <span>{quantity}</span>
-                <button onClick={() => addProductQuantity(item.id, 1)} disabled={(stockCalcul === 0)}>+</button>
+            <div className="itemInfo">
+                <Link to={`/Products/${item.name}`} className="img-back">
+                    <img src={item.imgURL} alt={item.name} />
+                </Link>
+                <div className="text-area">
+                    <h3>{item.name}</h3>
+                    <p>total = {item.price * quantity}$</p>
+
+                    <div className="controllQuantity">
+                        <button onClick={() => minusProductQuantity(item.id)}>-</button>
+                        <span>{quantity}</span>
+                        <button onClick={() => addProductQuantity(item.id, 1)} disabled={(stockCalcul === 0)}>+</button>
+                        <button className='trash'>
+                            <FontAwesomeIcon onClick={() => removeFromCart(item.id)} className="remove" icon={faTrash} />
+                            Remove
+                        </button>
+                    </div>
+                </div>
+
             </div>
-            <FontAwesomeIcon onClick={() => removeFromCart(item.id)} className="remove" icon={faXmark} />
+                  
         </div>
     </>
   )
